1b7e0252734ce6729aa3eecfe5d59259f3d9bf99
noor <noorul@creatiosoft.com>
2017-05-26 19:52:46 +0530

    added test script

---
+++ test.js
@@ -0,0 +1,25 @@
+/*
+ * @Author: noor
+ * @Date:   2017-05-26 19:42:07
+ * @Last Modified by:   noor
+ * @Last Modified time: 2017-05-26 19:52:00
+ */
+
+var winner = require('./index');
+var params = {
+    "boardCards": [
+        { "type": "heart", "rank": 1 },
+        { "type": "heart", "rank": 2 },
+        { "type": "heart", "rank": 11 },
+        { "type": "heart", "rank": 10 },
+        { "type": "heart", "rank": 13 }
+    ],
+    "playerCards": [{ "playerId": "1", "cards": [{ "type": "spade", "rank": 11 }, { "type": "club", "rank": 3 }] },
+        { "playerId": "2", "cards": [{ "type": "heart", "rank": 6 }, { "type": "club", "rank": 5 }] }
+    ]
+}
+
+console.log("Input:");
+console.log(JSON.stringify(params));
+console.log("Output:");
+console.log(JSON.stringify(winner(params)));
